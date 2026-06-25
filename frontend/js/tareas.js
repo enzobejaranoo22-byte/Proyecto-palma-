9f2a4372f5ba6c5ae7246a8f2ba2a1be9f0d9b31
enzobejaranoo22-byte <enzobejaranoo22@gmail>
2026-06-25 16:45:22 -0300

    palma

--- frontend/js/tareas.js
+++ frontend/js/tareas.js
@@ -1,11 +1,66 @@
+
 function agregartarea() {
-    let tarea= document.getElementById("nuevatarea").value;
-    if(tarea=== "") {
+    let tarea = document.getElementById("nuevatarea").value;
+
+    if (tarea == "") {
         alert("ingrese una nueva tarea");
         return;
     }
-    let li = document.createElement("li");
-    li.textContent = tarea;
-    document.getElementById("listatareas").prepend(li);
+
+    crearTarea(tarea);
+
+    guardarTareas();
+
     document.getElementById("nuevatarea").value = "";
+}
+
+
+// función para crear tarea con botón borrar
+function crearTarea(texto) {
+    let li = document.createElement("li");
+    li.textContent = texto;
+
+    let botonBorrar = document.createElement("button");
+    botonBorrar.textContent = " ❌";
+
+    botonBorrar.onclick = function () {
+        li.remove();
+        guardarTareas(); // actualiza localStorage
+    };
+
+    li.appendChild(botonBorrar);
+
+
+    document.getElementById("listatareas").prepend(li);
+}
+
+// guardar tareas
+function guardarTareas() {
+    let lista = [];
+
+    let tareas = document.querySelectorAll("#listatareas li");
+
+    tareas.forEach(function(item) {
+        // saca el texto sin incluir la X del botón
+        let texto = item.firstChild.textContent;
+
+        lista.push(texto);
+    });
+
+    localStorage.setItem("tareas", JSON.stringify(lista));
+}
+
+// cargar tareas al abrir página
+function cargarTareas() {
+    let tareasGuardadas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+    tareasGuardadas.forEach(function(tarea) {
+        crearTarea(tarea);
+    });
+}
+
+
+// ejecutar al iniciar
+window.onload = function () {
+    cargarTareas();
 }
